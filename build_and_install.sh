40e324bb0022461e18433c8de39669d240840d02
Bas Nijholt <basnijholt@gmail.com>
2016-02-18 11:41:14 +0100

    Small change to one-line build command

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,7 +1,6 @@
 conda config --set anaconda_upload yes
 conda config --add channels http://conda.anaconda.org/basnijholt
-conda build metis; conda build scotch; conda build lapack;
-conda build mumps_seq;
+conda build metis; conda build scotch; conda build mumps_seq;
 conda build tinyarray --python 3.5; conda build tinyarray --python 3.4; conda build tinyarray --python 2.7;
 conda build kwant --python 3.5; conda build kwant --python 3.4; conda build kwant --python 2.7;
 
